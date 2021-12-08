c9c9fc1b767a1767118000d48f82130191566ec5
yavuz <yavuz.akpinar.68@gmail.com>
2021-12-08 22:54:21 +0300

    feat: Navigation structure created

---
+++ src/navigation/Navigation.js
@@ -0,0 +1,18 @@
+import { NavigationContainer } from "@react-navigation/native";
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import MainPage from "../pages/MainPage"
+import DetailPage from "../pages/DetailPage";
+
+const Stack = createNativeStackNavigator()
+
+const Navigation = () => {
+  <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name = "MainPage" component={MainPage}></Stack.Screen>
+      <Stack.Screen name = "DetailPage" component={DetailPage}></Stack.Screen>
+    </Stack.Navigator>
+  </NavigationContainer>
+}
+export default Navigation
