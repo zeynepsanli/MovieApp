a682aed883caca50c6629141163f74289f65c836
yavuz <yavuz.akpinar.68@gmail.com>
2021-12-09 17:13:47 +0300

    fix: return error solved

--- src/components/Cards/MovieDetailCard.js
+++ src/components/Cards/MovieDetailCard.js
@@ -2,8 +2,9 @@ import React from "react";
 import { Text, View } from "react-native";
 
 const MovieDetailCard = () => {
+  return (
   <View>
     <Text>Movie Detail Card</Text>
   </View>
-}
+  )}
 export default MovieDetailCard
