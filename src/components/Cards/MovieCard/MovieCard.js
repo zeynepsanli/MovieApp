c544305feb5163dfafac12a3bdaf15a72ba7e9d9
yavuz <yavuz.akpinar.68@gmail.com>
2021-12-11 01:23:36 +0300

    feat: images added

--- src/components/Cards/MovieCard/MovieCard.js
+++ src/components/Cards/MovieCard/MovieCard.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, TouchableOpacity, View } from "react-native";
+import { ImageBackground, Text, TouchableOpacity, View } from "react-native";
 import styles from "./MovieCardStyle";
 import Icon from 'react-native-vector-icons/FontAwesome';
 
@@ -9,18 +9,22 @@ const MovieCard = ({item, onPress}) => {
   <TouchableOpacity 
     style= {styles.container}
     onPress={onPress}>
-    <View style={styles.textView}>
-        <Text style={styles.text}>{item.name}</Text>
-        <View>
-          <Text style={styles.text}>{item.rate}</Text>
-          <Icon name='star' size={25} color='yellow' />
-        </View>
-        {item.genre.map((gen, ind) => {
-          return (
-            <Text style={styles.text} key={ind} >{gen}</Text>
-          )
-        })}
-    </View>
+    <ImageBackground 
+      source={{uri:item.image}} 
+      style={styles.imageContainer}>
+      <View style={styles.textView}>
+          <Text style={styles.text}>{item.name}</Text>
+          <View style ={styles.rateView}>
+            <Text style={styles.text}>{item.rate}</Text>
+            <Icon name='star' size={25} color='yellow' />
+          </View>
+            {item.genre.map((gen, ind) => {
+            return (
+              <Text style={styles.text} key={ind} >{gen}</Text>
+            )
+          })}
+      </View>
+    </ImageBackground>
   </TouchableOpacity>
   )}
 export default MovieCard
--- src/components/Cards/MovieDteailCard/MovieDetailCard.js
+++ src/components/Cards/MovieDteailCard/MovieDetailCard.js
@@ -1,23 +1,30 @@
 import React from "react";
-import { ScrollView, Text, View, Image } from "react-native";
+import { ScrollView, Text, View, Image, ImageBackground } from "react-native";
 import styles from "./MovieDetailCardStyle";
 
 const MovieDetailCard = ({item}) => {
   return (
-    <View style={styles.body_container}>
-      <View>
-        <Image />
-      </View>
+    <View style={styles.container}>
+      
       <View style={styles.genre}>
         {item.genre.map((gen, ind) => {
-          return (<View style={styles.genre_index} key={ind}>
-            <Text key={ind} >{gen}</Text></View>
+          return (
+          <View style={styles.genre_index} key={ind}>
+            <Text key={ind} >{gen}</Text>
+          </View>
           )
         })}
       </View>
-      <View style={styles.brief} >
-        <Text style={styles.title} >{item.name}</Text>
-        <Text style={styles.text} >{item.brief}</Text>
+      <View style={styles.body} >
+        <ImageBackground 
+          style={styles.image}
+          source={{uri:item.image}}>
+        <View style={styles.briefView}>
+          <Text style={styles.title} >{item.name}</Text>
+          <Text style={styles.text} >{item.brief}</Text>
+          <Text style={styles.text}>{item.rate}</Text>
+        </View>
+        </ImageBackground>
       </View>
       <View style={styles.cast} >
         <Text>DIRECTOR: {item.director}</Text>
@@ -27,9 +34,7 @@ const MovieDetailCard = ({item}) => {
             <Text key={ind} >{cast}</Text>
           )
         })}
-
       </View>
-
     </View>
 
 
