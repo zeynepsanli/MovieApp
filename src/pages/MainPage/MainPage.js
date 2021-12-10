6dc20ca8f075b1c42bee918db442f07cab40c485
yavuz <yavuz.akpinar.68@gmail.com>
2021-12-11 01:26:37 +0300

    feat: Filter structure is missing

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,14 +1,21 @@
 import { useNavigation } from "@react-navigation/native";
-import React from "react";
-import { ActivityIndicator, FlatList, Text, View } from "react-native";
+import React, { useState } from "react";
+import { ActivityIndicator, FlatList, SafeAreaView, Text, View, ScrollView } from "react-native";
+import GenreButton from "../../components/Button/genreButton";
 import MovieCard from "../../components/Cards/MovieCard/MovieCard";
 import useFetch from "../../hooks/useFetch";
+import styles from "./MainPageStyles";
+import genres from "../../components/Genres/genres.json"
 
 
 const MainPage = () => {
   const {data, loading} = useFetch("")
   const navigation = useNavigation()
+  const [genre, setGenre] = useState("")
 
+  const handleGenreFilter = () => {
+  setGenre("ACTION")
+  }
 
   const handleRenderItem = ({ item }) => (
     <MovieCard item={item} onPress={() => navigation.navigate("DetailPage", {id:item})}/>
@@ -17,14 +24,31 @@ const MainPage = () => {
   const handleDetail = ({item}) => {
     
   }
+    
+  let source = data.filter(e => e.genre.includes(genre)) ?? "";
   return(
-  <View style={{alignItems:"center"}}>{loading ? <ActivityIndicator />:
-    <FlatList 
-      data={data} 
-      renderItem={handleRenderItem}
-      numColumns={2}
-      horizontal={false} />
-  }
-  </View>
+  <SafeAreaView style={styles.container}>
+    <View style={styles.button_View}>
+      <GenreButton style = {styles.button} genreName={"ACTION"} onPress={handleGenreFilter}/>
+      <GenreButton style = {styles.button} genreName={"COMEDY"} onPress={handleGenreFilter}/>
+      <GenreButton style = {styles.button} genreName={"HORROR"} onPress={handleGenreFilter}/>
+      <GenreButton style = {styles.button} genreName={"DRAMA"} onPress={handleGenreFilter}/>
+    </View> 
+    <View style={styles.button_View}> 
+      <GenreButton style = {styles.button} genreName={"FANTASY"} onPress={handleGenreFilter}/>
+      <GenreButton style = {styles.button} genreName={"ROMANCE"} onPress={handleGenreFilter}/>
+      <GenreButton style = {styles.button} genreName={"THRILLER"} onPress={handleGenreFilter}/>
+      <GenreButton style = {styles.button} genreName={"ALL GENRES"} onPress={handleGenreFilter}/>
+    </View>
+    <View style={{alignItems:"center", flex:15}}>{loading ? <ActivityIndicator />:
+      <FlatList 
+        data={data} 
+        renderItem={handleRenderItem}
+        numColumns={2}
+        horizontal={false} />
+    }
+    </View>
+    
+  </SafeAreaView>
   )}
 export default MainPage
