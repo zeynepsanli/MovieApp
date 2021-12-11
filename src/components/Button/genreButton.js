cf07c987bf7a3d7f92085b4d95e86fd9a66d7daa
yavuz <yavuz.akpinar.68@gmail.com>
2021-12-11 19:22:37 +0300

    feat: onPress prop given properly

--- src/components/Button/genreButton.js
+++ src/components/Button/genreButton.js
@@ -2,11 +2,11 @@ import React from "react";
 import { TouchableOpacity, Text } from "react-native";
 import styles from "./genreButtonstyle";
 
-const GenreButton = ({genreName, onPress, index}) => {
+const GenreButton = ({genreName, onPress, active }) => {
   return(
     <TouchableOpacity
       style={styles.button}
-      onPress={(genreName) => onPress(genreName)}>
+      onPress={() => onPress(genreName)}>
       <Text style={styles.text}>{genreName}</Text>
     </TouchableOpacity>
   )
