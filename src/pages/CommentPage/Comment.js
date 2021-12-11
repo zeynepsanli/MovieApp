452aaabfbec8652dc80aa31bca2eb629bc2dcb75
yavuz <yavuz.akpinar.68@gmail.com>
2021-12-11 21:57:23 +0300

    feat: Comment page is now ready

--- src/pages/CommentPage/Comment.js
+++ src/pages/CommentPage/Comment.js
@@ -1,9 +1,53 @@
-import React from "react";
-import { Text } from "react-native";
+import { useRoute } from "@react-navigation/native";
+import React, { useState } from "react";
+import { FlatList, SafeAreaView, Text, View } from "react-native";
+import CommentButton from "../../components/Button/commentButton";
+
+import CommentCard from "../../components/Cards/ComentCard/CommentCard";
+import Input from "../../components/Input/Input";
+import useFetch from "../../hooks/useFetch";
+import styles from "./CommentStyle";
 
 const Comment = () => {
+  const route = useRoute()
+  const {info} = route.params
+  const id = route.params.info.id
+  const {data, loading} = useFetch("comments")
+  const [source = data.filter(a => a.movieId.includes(id)), setSource] = useState(source) ;
+  const [keyword, setKeyword] = useState("")
+  
+
+  
+
+  const addComment = () => {
+    setSource([{keyword:keyword}, ...source])
+    handleState()
+  }
+
+  const handleState = () => {
+    setKeyword("")
+  }
+
   return(
-    <Text>yoryum</Text>
+    <SafeAreaView style={styles.container}>
+      
+      <View style={styles.listView}>
+        {source.length > 0 ? <FlatList data={source} renderItem={({item}) => <CommentCard item={item}/>}/>
+        :<Text>Leave The First Comment</Text>}
+      </View>
+      <View style={styles.inputView}>
+        <View style={styles.buttonView}>
+          <CommentButton
+            onPress={addComment}
+            size={25} 
+            text="Leave a comment"/>
+        </View>
+        <Input
+          placeholder={"Comment"}
+          value={keyword}
+          onChangeText={setKeyword}/>
+      </View>
+    </SafeAreaView>
   )
 }
 export default Comment
---
+++ src/pages/CommentPage/CommentStyle.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1
+  },
+  listView:{
+    flex:7
+  },
+  buttonView:{
+    flex:1
+  },
+  inputView:{
+    flexDirection:"row",
+    flex:1,
+    alignItems:"center"
+  }
+})
+export default styles
