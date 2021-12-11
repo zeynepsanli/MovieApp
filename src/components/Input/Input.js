2dd4d2a8d1a1ef250715e76b7ea2a274a9a6d256
yavuz <yavuz.akpinar.68@gmail.com>
2021-12-11 21:56:14 +0300

    feat: Input component created

---
+++ src/components/Input/Input.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { TextInput, View } from "react-native";
+import styles from "./InputStyle";
+
+const Input = ({onChangeText, value, placeholder}) => {
+  return(
+  <View style={styles.input}>
+    <TextInput
+      onChangeText={onChangeText}
+      placeholder={placeholder}
+      value={value}
+      />
+  </View>
+  )
+}
+
+export default Input
---
+++ src/components/Input/InputStyle.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  input:{
+    margin:10,
+    borderRadius:15,
+    backgroundColor:"grey",
+    flex:3
+  }
+})
+
+export default styles
