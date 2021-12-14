0e15be7f471a27003ebf093cf6164d022833597c
yavuz <yavuz.akpinar.68@gmail.com>
2021-12-14 15:51:04 +0300

    feat: Activityindicator added

--- src/pages/CommentPage/Comment.js
+++ src/pages/CommentPage/Comment.js
@@ -1,6 +1,6 @@
 import { useRoute } from "@react-navigation/native";
 import React, { useState } from "react";
-import { FlatList, SafeAreaView, Text, View } from "react-native";
+import { ActivityIndicator, FlatList, SafeAreaView, Text, View } from "react-native";
 import CommentButton from "../../components/Button/commentButton";
 
 import CommentCard from "../../components/Cards/ComentCard/CommentCard";
@@ -14,7 +14,7 @@ const Comment = () => {
   const id = route.params.info.id
   const { data, loading } = useFetch("comments")
 
-  const [source = data.filter(a => a.movieId.includes(id)), setSource] = useState(source);
+  const [source = data.filter(a => a.movieId.includes(id)), setSource] = useState();
   const [keyword, setKeyword] = useState("");
 
   const addComment = () => {
@@ -26,14 +26,18 @@ const Comment = () => {
     setKeyword("")
   }
 
+  if (loading === true) {
+    return <ActivityIndicator />
+  }
+
   return (
     <SafeAreaView style={styles.container}>
-
       <View style={styles.listView}>
-        {source.length > 0 ? <FlatList
-          data={source}
-          renderItem={({ item }) => <CommentCard item={item} />} />
-          : <Text>Leave The First Comment</Text>}
+        {source.length > 0 
+          ? <FlatList
+              data={source}
+              renderItem={({ item }) => <CommentCard item={item} />} />
+          : <Text style={styles.text}>Leave The First Comment</Text>}
       </View>
       <View style={styles.inputView}>
         <View style={styles.buttonView}>
